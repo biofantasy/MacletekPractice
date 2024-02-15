3c279423bce621eae1ee53f8038d18f30ec20330
paul.chen <paul.chen@macletek.com>
2024-02-15 19:08:48 +0800

    init

---
+++ practice/src/main/java/com/macletek/practice/PracticeApplication.java
@@ -0,0 +1,21 @@
+package com.macletek.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class PracticeApplication {
+
+	@RequestMapping("/")
+    String index() {
+        return "hello spring boot";
+    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeApplication.class, args);
+	}
+
+}
